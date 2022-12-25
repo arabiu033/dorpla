73fe33f8f037ddba9d4f21d789365caba4fb7fd6
arabiu033 <arabiu033@gmail.com>
2022-12-25 07:00:22 -0800

    Setup the termianl reading to support raw mode

--- core/src/main.c
+++ core/src/main.c
@@ -6,5 +6,15 @@
  */
 int main()
 {
+	char ch;
+
+	enable_raw_mode();
+	while(1)
+	{
+		ch = '\0';
+		if (read(STDIN_FILENO, &ch, 1) == -1 && errno != EAGAIN) die("read");
+		iscntrl(ch) ? printf("%d\r\n", ch) : printf("%d ('%c')\r\n", ch, ch);
+		if (ch == 'q') break;
+	}
 	return (0);
 }
