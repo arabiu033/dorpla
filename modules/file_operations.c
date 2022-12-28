6006341ef1726c70711bc9ce93b5efc52739efd0
arabiu033 <arabiu033@gmail.com>
2022-12-28 07:24:37 -0800

    Handle vertical and horizontal scrolling

--- modules/file_operations.c
+++ modules/file_operations.c
@@ -41,4 +41,57 @@ void append_row(char *s, size_t len)
 	memcpy(E.row[at].line, s, len);
 	E.row[at].line[len] = '\0';
 	E.numrows++;
+	E.row[at].rsize = 0;
+	E.row[at].render = NULL;
+	update_row(&E.row[at]);
+}
+
+/**
+ * update_row - handle row with special characters like tabs
+ * @row: the row to handle
+ * Return: void - nothing
+ */
+void update_row(erow *row)
+{
+	int i, indx, tabs = 0;
+	for (i = 0; i < row->size; i++)
+	{
+		if (row->line[i] == '\t') ++tabs;
+	}
+	free(row->render);
+	row->render = malloc(row->size + tabs * (TAB_STOP - 1) + 1);
+	indx = 0;
+	for (i = 0; i < row->size; i++)
+	{
+		if (row->line[i] == '\t')
+		{
+			row->render[indx++] = ' ';
+			while (indx % TAB_STOP) row->render[indx++] = ' ';
+		}
+		else row->render[indx++] = row->line[i];
+	}
+
+	row->render[indx] = '\0';
+	row->rsize = indx;
+}
+
+/**
+ * row_cx_to_rx - calculate the cursor position taking tabs into account
+ * @row: the current row to calculate
+ * @cx: the current cursor positon
+ * Return: the new size inluding tabs spaces
+ */
+int row_cx_to_rx(erow *row, int cx)
+{
+	int i, rx = 0;
+	for (i = 0; i < cx; i++)
+	{
+		if (row->line[i] == '\t')
+		{
+			rx += (TAB_STOP - 1) - (rx % TAB_STOP);
+		}
+		++rx;
+	}
+
+	return (rx);
 }
